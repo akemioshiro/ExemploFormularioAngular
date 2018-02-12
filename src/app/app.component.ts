3ca5c7edfa7b1447ac2c844ce953946d42f5a2fb
patricia.oshiro@totvs.com.br <patricia.oshiro@totvs.com.br>
2018-02-12 21:08:54 -0200

    exemplo de formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Mock } from './mock';
+import { Person } from "./model";
+
 @Component({
   selector: 'app-root',
+  template: '<h1>Exemplo de formulario</h1>',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [Mock]
 })
 export class AppComponent {
-  title = 'app';
+  user: Person;
+
+  constructor(_mock:Mock) {
+    this.user = _mock.mike;
+  }
 }
